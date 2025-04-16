1650fe4f2e0eb6c9c4fc7524277aa22c7d6346e1
faraamalia <frarsa18@gmail.com>
2025-04-16 18:57:22 +0700

    Refactor Class Employee: Duplikasi code pada method setMonthlySalary serta modifikasi kembali class Employee

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -4,7 +4,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Employee {
-
     private String employeeId;
     private String firstName;
     private String lastName;
@@ -25,46 +24,29 @@ public class Employee {
     private List<String> childNames;
     private List<String> childIdNumbers;
 
-    public Employee(String employeeId, String firstName, String lastName, String idNumber, String address,
-                    int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, Gender gender) {
-
+    public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, EmployeeJoinDate joinDate, boolean isForeigner, Gender gender) {
         this.employeeId = employeeId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.idNumber = idNumber;
         this.address = address;
-        this.joinDate = new EmployeeJoinDate(yearJoined, monthJoined, dayJoined);
+        this.joinDate = joinDate;
         this.isForeigner = isForeigner;
         this.gender = gender;
 
-        this.childNames = new LinkedList<>();
-        this.childIdNumbers = new LinkedList<>();
+        childNames = new LinkedList<>();
+        childIdNumbers = new LinkedList<>();
     }
 
-    public void setMonthlySalary(int grade) {
-        if (grade == 1) {
-            monthlySalary = 3000000;
-            if (isForeigner) {
-                monthlySalary = (int) (3000000 * 1.5);
-            }
-        } else if (grade == 2) {
-            monthlySalary = 5000000;
-            if (isForeigner) {
-                monthlySalary = (int) (3000000 * 1.5);
-            }
-        } else if (grade == 3) {
-            monthlySalary = 7000000;
-            if (isForeigner) {
-                monthlySalary = (int) (3000000 * 1.5);
-            }
-        }
+    public void setMonthlySalary(int grade) {    
+        this.monthlySalary = CalculateSalary.calculateSalaryForGrade(grade, isForeigner);
     }
 
-    public void setAnnualDeductible(int deductible) {
+    public void setAnnualDeductible(int deductible) {    
         this.annualDeductible = deductible;
     }
 
-    public void setAdditionalIncome(int income) {
+    public void setAdditionalIncome(int income) {    
         this.otherMonthlyIncome = income;
     }
 
@@ -80,13 +62,6 @@ public class Employee {
 
     public int getAnnualIncomeTax() {
         int monthsWorked = joinDate.calculateMonthsWorkedInYear();
-        return TaxFunction.calculateTax(
-                monthlySalary,
-                otherMonthlyIncome,
-                monthsWorked,
-                annualDeductible,
-                spouseIdNumber == null || spouseIdNumber.isEmpty(),
-                childIdNumbers.size()
-        );
+        return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthsWorked, annualDeductible, spouseIdNumber != null, childIdNumbers.size());
     }
 }
